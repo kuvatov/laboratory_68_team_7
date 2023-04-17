a7f47913515777f1d799a269af71c2a12a6b1013
romeeeoo <tarassenko@me.com>
2023-04-18 00:31:56 +0300

    logout and profile link on navbar

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from accounts.views import RegisterView, LoginView, ProfileView, UserChangeView
-
+from accounts.views import RegisterView, LoginView, ProfileView, UserChangeView, logout_view
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
     path("account/<int:pk>/", ProfileView.as_view(), name="account"),
     path("update/<int:pk>/", UserChangeView.as_view(), name="user_update")
 ]
--- source/accounts/views/__init__.py
+++ source/accounts/views/__init__.py
@@ -2,3 +2,4 @@ from .register import RegisterView
 from .login import LoginView
 from .profile_detailed import ProfileView
 from .update_profile import UserChangeView
+from .logout import logout_view
