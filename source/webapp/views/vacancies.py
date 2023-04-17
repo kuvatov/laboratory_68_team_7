073020d01d11827a9ae5ac8d69cc8370382961cc
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-17 13:43:46 +0600

    #4 Добавил функционал редактирования вакансии

--- source/webapp/views/vacancies.py
+++ source/webapp/views/vacancies.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from webapp.models import Vacancy
 
@@ -16,3 +16,15 @@ class VacancyCreateView(LoginRequiredMixin, CreateView):
     template_name = 'vacancy/vacancy_create.html'
     fields = ('title', 'salary', 'description', 'experience_from', 'experience_to', 'category', 'is_published')
     success_url = reverse_lazy('vacancy_list')
+
+
+class VacancyUpdateView(LoginRequiredMixin, UpdateView):
+    model = Vacancy
+    fields = ('title', 'salary', 'description', 'experience_from', 'experience_to', 'category', 'is_published')
+    template_name = 'vacancy/vacancy_update.html'
+    success_url = reverse_lazy('vacancy_list')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save(update_fields=['updated_at'])
+        return super().form_valid(form)
