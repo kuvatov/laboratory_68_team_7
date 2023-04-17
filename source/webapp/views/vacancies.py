cbf29d24a7a839fffc2fb9dde567ca7f05243d50
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-17 13:03:43 +0600

    #2 Добавил функционал создания вакансии, а также добавил форму создания/редактирования вакансии

--- source/webapp/views/vacancies.py
+++ source/webapp/views/vacancies.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from webapp.models import Vacancy
 
@@ -7,3 +9,10 @@ class VacancyListView(ListView):
     model = Vacancy
     template_name = 'vacancy/vacancy_list.html'
     context_object_name = 'vacancies'
+
+
+class VacancyCreateView(LoginRequiredMixin, CreateView):
+    model = Vacancy
+    template_name = 'vacancy/vacancy_create.html'
+    fields = ('title', 'salary', 'description', 'experience_from', 'experience_to', 'category', 'is_published')
+    success_url = reverse_lazy('vacancy_list')
