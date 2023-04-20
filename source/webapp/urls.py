95ff9316540d269334829fb256f748b1e475d6cd
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-20 15:24:39 +0600

    Разрешил конфликты при слиянии ветки dev с веткой employer

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from webapp.views import CreateCV
 from webapp.views.vacancies import VacancyListView, VacancyCreateView, VacancyUpdateView, VacancyUpdateDateView
 
 urlpatterns = [
     path('vacancies', VacancyListView.as_view(), name='vacancy_list'),
     path('vacancies/create/', VacancyCreateView.as_view(), name='vacancy_create'),
     path('vacancies/<int:pk>/update/', VacancyUpdateView.as_view(), name='vacancy_update'),
-    path('vacancies/<int:pk>/update_date/', VacancyUpdateDateView.as_view(), name='vacancy_update_date')
+    path('vacancies/<int:pk>/update_date/', VacancyUpdateDateView.as_view(), name='vacancy_update_date'),
+    path('cv/create/', CreateCV.as_view(), name="cv_create")
 ]
