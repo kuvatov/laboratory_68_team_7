f7c207f1ea4efbd9bee5096e79c7a701d3ae1e3b
romeeeoo <tarassenko@me.com>
2023-04-20 20:26:59 +0300

    разрешение на добавление eductaion в резюме по автору

--- source/webapp/views/create_education.py
+++ source/webapp/views/create_education.py
@@ -1,30 +1,28 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect
 from django.views.generic import CreateView
-# from django.contrib.auth.mixins import UserPassesTestMixin
 
 from webapp.models import Education, CV
 
 
-class CreateEducation(CreateView):
+class CreateEducation(UserPassesTestMixin, CreateView):
     template_name = "CV/education_create.html"
     model = Education
     fields = ("graduation_date", "institution_name", "specialisation")
 
     def post(self, request, *args, **kwargs):
         form = self.get_form_class()(request.POST)
-        print(kwargs)
-        print(args)
         pk = kwargs.get("pk")
-        print(form.is_valid())
         if form.is_valid():
             self.object = form.save()
-            self.object.CVs.set(CV.objects.filter(pk=pk))
+            self.CV_object = CV.objects.filter(pk=pk)
+            self.object.CVs.set(self.CV_object)
             return redirect("cv_detailed", pk)
         else:
             return super().post(request, *args, **kwargs)
 
-    # def test_func(self):
-    #     if self.request.user.profile.is_corporate:
-    #         return False
-    #     else:
-    #         return True
+    def test_func(self):
+        if self.request.user.CVs.filter(pk=self.kwargs.get("pk")):
+            return True
+        else:
+            return False
