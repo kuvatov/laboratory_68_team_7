3e0cd0b4e2697c1f67fbc695f57c6a3e6648c370
romeeeoo <tarassenko@me.com>
2023-04-21 12:23:21 +0300

    is_corporate.js fixed

--- source/static/js/is_corporate.js
+++ source/static/js/is_corporate.js
@@ -2,6 +2,7 @@ const isCorporate = document.getElementById("id_is_corporate")
 const secondNameField = document.getElementById("second_name_field")
 const firstNameLabel = document.getElementById("first_name_label")
 const companyNameLabel = document.getElementById("company_name_label")
+const secondNameInput = document.getElementById("last_name_input")
 
 if (isCorporate) {
     function changeLabel() {
@@ -9,6 +10,7 @@ if (isCorporate) {
             firstNameLabel.style.display = "none"
             companyNameLabel.style.display = "inline"
             secondNameField.style.display="none"
+            secondNameInput.value = ""
         }
         else {
             firstNameLabel.style.display = "inline"
@@ -17,3 +19,5 @@ if (isCorporate) {
         }
     }
 }
+
+isCorporate.addEventListener("click", changeLabel)
